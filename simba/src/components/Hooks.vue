5cb6ef96064c65ef7b966ab1dbae89f17aa8644c
GnomeApprenti <lchakroun@outlook.fr>
2021-04-13 12:34:26 -0500

    Hooksforce

---
+++ simba/src/components/Hooks.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+
+<script>
+
+export default{
+  name: 'Hooks',
+  props: {
+    msg: String
+  },
+  beforeCreate() { console.log('Je ne suis pas encore  créé') },
+  created() { console.log('Je suis créé !') },
+  beforeMount() { console.log('Je vais bientôt être monté sur le DOM!') },
+  mounted() { console.log('Je suis monté sur le DOM!') },
+  beforeDestroy() { console.log('Je suis sur le point de disparaître du DOM !') },
+  destroyed() { console.log('Je suis supprimé') }
+}
+</script>
--- simba/src/views/About.vue
+++ simba/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Hooks msg="Composant pour tester les hooks (voir console)"/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Hooks from '@/components/Hooks.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Hooks
+  }
+}
+</script>
